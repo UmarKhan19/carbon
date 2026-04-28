e38b67149f64c537a351054c982b94158867b73f
Gaurav Bhatt <bhattgauravd@gmail.com>
2026-04-28 14:15:35 +0530

    Fix storage type multiselect issue

--- apps/erp/app/modules/inventory/inventory.models.ts
+++ apps/erp/app/modules/inventory/inventory.models.ts
@@ -170,7 +170,7 @@ export const storageUnitValidator = z.object({
   locationId: z.string().min(1, { message: "Location ID is required" }),
   warehouseId: zfd.text(z.string().optional()),
   parentId: zfd.text(z.string().optional()),
-  storageTypeIds: zfd.repeatableOfType(z.string()).optional()
+  storageTypeIds: zfd.repeatableOfType(z.string()).default([])
 });
 
 export const storageTypeValidator = z.object({
