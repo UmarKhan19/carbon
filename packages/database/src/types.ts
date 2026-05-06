20b7457fbb5110dbbd9ebb9e067bc3b628ad3c53
Sidwebworks <58144379+sidwebworks@users.noreply.github.com>
2026-05-07 01:57:47 +0530

    fix: lint issues

--- packages/database/src/types.ts
+++ packages/database/src/types.ts
@@ -35796,6 +35796,48 @@ export type Database = {
           },
         ]
       }
+      searchIndex_d7tp3mb9o0m059r81vf0: {
+        Row: {
+          createdAt: string
+          description: string | null
+          entityId: string
+          entityType: string
+          id: number
+          link: string
+          metadata: Json | null
+          searchVector: unknown
+          tags: string[] | null
+          title: string
+          updatedAt: string | null
+        }
+        Insert: {
+          createdAt?: string
+          description?: string | null
+          entityId: string
+          entityType: string
+          id?: number
+          link: string
+          metadata?: Json | null
+          searchVector?: unknown
+          tags?: string[] | null
+          title: string
+          updatedAt?: string | null
+        }
+        Update: {
+          createdAt?: string
+          description?: string | null
+          entityId?: string
+          entityType?: string
+          id?: number
+          link?: string
+          metadata?: Json | null
+          searchVector?: unknown
+          tags?: string[] | null
+          title?: string
+          updatedAt?: string | null
+        }
+        Relationships: []
+      }
       searchIndexRegistry: {
         Row: {
           companyId: string
