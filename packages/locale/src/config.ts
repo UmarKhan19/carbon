67f44b9ccfc95991a0f3b04d7835ac7fd13f6eee
Brad Barbin <bradbarbin@protonmail.com>
2026-05-12 23:47:47 -0400

    feat: add nl to supported locales

--- packages/locale/src/config.ts
+++ packages/locale/src/config.ts
@@ -11,7 +11,8 @@ export const supportedLanguages = [
   "pt",
   "ru",
   "zh",
-  "hi"
+  "hi",
+  "nl"
 ] as const;
 export type SupportedLanguage = (typeof supportedLanguages)[number];
 
@@ -48,7 +49,8 @@ export const languageNativeLabels: Record<SupportedLanguage, string> = {
   pt: "Português",
   ru: "Русский",
   zh: "中文",
-  hi: "हिन्दी"
+  hi: "हिन्दी",
+  nl: "Nederlands"
 };
 
 /**
