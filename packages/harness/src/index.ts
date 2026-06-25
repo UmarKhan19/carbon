de469a6dd5e19cd474221b5ecb94b81d0f00d3df
Brad Barbin <bradbarbin@protonmail.com>
2026-06-25 01:19:47 -0400

    feat(harness): barrel + README

--- packages/harness/src/index.ts
+++ packages/harness/src/index.ts
@@ -1,1 +1,9 @@
-export {};
+export { type Binding, type LoopKind, parseBinding } from "./binding";
+export {
+  type Exec,
+  FLOOR_GATES,
+  type Gate,
+  type GateResult,
+  runGates
+} from "./gates";
+export { appendLedger, type LedgerEntry, readLedger } from "./ledger";
